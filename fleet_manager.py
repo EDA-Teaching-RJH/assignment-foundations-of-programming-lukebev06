870d4f10672717725a7fa3a0d3c632926e6b8aa5
lukebev06 <lmb73@kent.ac.uk>
2026-02-01 19:42:26 +0000

    .index error

--- fleet_manager.py
+++ fleet_manager.py
@@ -1,19 +1,22 @@
 
 def main():
 
-    print("BOOTING SYSTEM...")
-    print("...")
-    print("WELCOME TO FLEET COMMAND")
+ names = ["Picard","Spock","Geordi","Kathryn","Riker"]
+ ranks = ["Captain","Commander","Lt. commander","Captain","Commander"]
+ divisions = ["Command","Science","Operations","Science","Command"]
+ IDs = ["231-427-1701","179-425-2267","452-199-639","746-358-74656","402-857-935"]
 
-    def init_database(names,ranks,divisions,IDs):
-        names = ["Picard","Spock","Geordi","Kathryn","Riker"]
-        ranks = ["Captain","Commander","Lt. commander","Captain","Commander"]
-        divisions = ["Command","Science","Operations","Science","Command"]
-        IDs = ["231-427-1701","179-425-2267","452-199-639","746-358-74656","402-857-935"]
-        for i in range(len(names)):
-            print(f"{names[i]} - {ranks[i]} - {divisions[i]} - {IDs[i]}")
 
-    def display_menu():
+ print("BOOTING SYSTEM...")
+ print("...")
+ print("WELCOME TO FLEET COMMAND")
+
+ def init_database():
+        
+    for i in range(len(names)):
+        print(f"{names[i]} - {ranks[i]} - {divisions[i]} - {IDs[i]}")
+
+ def display_menu():
         name = input("what is your full name: ")
         print(f"\n--- MENU --- {name} ---")
         print("1. View Crew")
@@ -27,39 +30,36 @@ def main():
         elif option == "2":
             add_members()
 
-    def add_members(names,ranks,divisions,IDs):
+ def add_members():
         all_ranks = ["Captain","Commander","Lt. Commander","Lieutenant","Lt. junior","Ensign"]
 
         new_name = input("Name: ")
         new_division = input("Div: ")
         
-
+        n = 0 
         while n != 1 :
-            n = 0
+        
             new_id = input("enter a unique id (in the format xxx-xxx-xxx): ")
             print("\n---enter one of the valid ranks---")
             print("Captain ,Commander ,Lt. Commander ,Lieutenant ,Lt. junior ,Ensign")
             new_rank = input("Rank:")
 
-            if IDs.index(new_id, 0 ) < 0 and all_ranks.index(new_rank,0) >=0: 
-                names.append(new_name)
-                ranks.append(new_rank)
-                divisions.append(new_division)
-                IDs.append(new_id)
-                n = 1
-            elif all_ranks.index(new_rank,0) == "":
-                print("that rank is incorrect. please try again.")
+            if all_ranks.index(new_rank,0) >=0: 
+                if IDs.index(new_id,0) >=0:
+                    print("that id already exists please try again. ")
 
-            elif IDs.index(new_id,0) >= 0:
-                print("that id is in use. please select another one")
+                else:
+                    names.append(new_name)
+                    ranks.append(new_rank)
+                    divisions.append(new_division)
+                    IDs.append(new_id)
+                    n = 1
+            else:
+                print("That rank doesnt exist please try again. ")
+            
         
-        # cannot compare a null resilt from index use different method. for future reference.
+        #if .index cant find result it gives error
 
 
-
-
-
-
-
-    display_menu()
+ display_menu()
 main()
